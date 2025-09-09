49743644790b624546831118924977bc3f98242f
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-09-09 17:49:08 +0600

    feat: use file system as db and created CRUD method functions

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environments = require("./helpers/environments");
 
 const app = {};
-
-// config
-app.config = {
-	port: 8000,
-};
 
 // create server
 app.createServer = () => {
 	const server = http.createServer(app.handleReqRes);
-	server.listen(app.config.port, () => {
-		console.log(`Server is listening on ${app.config.port}`);
+	server.listen(environments.port, () => {
+		console.log(`Server is listening on ${environments.port}`);
 	});
 };
 
